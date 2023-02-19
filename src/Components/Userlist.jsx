45a49b14958d29401cede2600b54e5982c1300b5
Niranjan360 <connect2hemanthsimha@gmail.com>
2023-02-19 12:43:55 +0530

    second update

--- src/Components/Userlist.jsx
+++ src/Components/Userlist.jsx
@@ -1,5 +1,16 @@
 
-const Userlist = ({data}) => {
+const Userlist = ({data ,edited,setEdited}) => {
+
+    let handleLiked = (id , value)=>{
+        let userlist = JSON.parse(localStorage.getItem("userlist"));
+        let ind = userlist.findIndex((user)=>{ return user.id===id});
+        userlist[ind].liked = value;
+        userlist = JSON.stringify(userlist)
+        localStorage.setItem("userlist" , userlist);
+        console.log(JSON.parse(localStorage.getItem("userlist")));
+        setEdited(edited+1);
+    }
+
 
     return ( 
         <div className="users-list">
@@ -9,13 +20,14 @@ const Userlist = ({data}) => {
                         <div className="user" key={user.id}>
                             <img src={`https://avatars.dicebear.com/v2/avataaars/%7b%7b${user.name}%7d%7d.svg?options%5bmood%5d%5b%5d=happy`} alt="user pic" />
                             <h3>{user.name}</h3>
-                            <p><i class='bx bx-envelope'></i> {user.email}</p>
-                            <p><i class='bx bx-phone' ></i> {user.phone}</p>
-                            <p><i class='bx bx-globe'></i> {user.website}</p>
+                            <p><i className='bx bx-envelope'></i> {user.email}</p>
+                            <p><i className='bx bx-phone' ></i> {user.phone}</p>
+                            <p><i className='bx bx-globe'></i> {user.website}</p>
                             <div className="actions">
-                                <button><i class='bx bx-heart' ></i></button>
-                                <button><i class='bx bx-edit-alt' ></i></button>
-                                <button><i class='bx bx-message-square-x'></i></button>
+                                {!user.liked && <button onClick={()=>{handleLiked(user.id , true)}}><i className='bx bx-heart' ></i></button>}
+                                {user.liked  && <button onClick={()=>{handleLiked(user.id , false)}}><i className='bx bxs-heart' style={{color:"red"}}></i></button>}
+                                <button><i className='bx bx-edit-alt' ></i></button>
+                                <button><i className='bx bx-message-square-x'></i></button>
                             </div>
                         </div>
                     )
